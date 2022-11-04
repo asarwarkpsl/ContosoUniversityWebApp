c8f99942f9083acd9ebc4540a68458213f40ec78
Azeem Sarwar <asarwar.kpsl@gmail.com>
2022-11-04 15:48:50 +0500

    working on courses api

---
+++ ContosoUniversity.API/Controllers/Courses/CoursesController.cs
@@ -0,0 +1,51 @@
+﻿using Data.Repository;
+using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContosoUniversity.API.Controllers.Courses
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseRepository _courseRepo;
+
+        public CoursesController(ICourseRepository courseRepo)
+        {
+            _courseRepo = courseRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> GetAllUsers()
+        {
+            return Ok(await _courseRepo.GetCoursesAsync());
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- ContosoUniversity.API/Program.cs
+++ ContosoUniversity.API/Program.cs
@@ -1,11 +1,23 @@
+using Data.Context;
+using Data.Models;
+using Data.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SchoolContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext")));
+
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
 
 var app = builder.Build();
 
@@ -21,5 +33,16 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<SchoolContext>();
+
+    context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
+}
 
 app.Run();
