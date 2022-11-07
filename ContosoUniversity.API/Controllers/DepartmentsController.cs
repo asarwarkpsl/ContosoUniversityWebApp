d94312ba65113e89ff2038cc4dc9a5ed4ab21cb7
Azeem Sarwar <asarwar.kpsl@outlook.com>
2022-11-07 14:58:59 +0500

    dept controller complete

---
+++ ContosoUniversity.API/Controllers/DepartmentsController.cs
@@ -0,0 +1,72 @@
+﻿using ContosoUniversity.Data.Repository;
+using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContosoUniversity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IDepartmentRepository _repo;
+
+        public DepartmentsController(IDepartmentRepository repo)
+        {
+            _repo = repo;
+        }
+        // GET: api/<DepartmentsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Department>>> Get()
+        {
+            return Ok(await _repo.GetDepartmentsAsync());
+        }
+
+        // GET api/<DepartmentsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Department>> GetDepartmentByID(int id)
+        {
+            var department = await _repo.GetDepartmentAsync(id);
+
+            if (department == null)
+                return NotFound();
+
+            return department;
+        }
+
+        // POST api/<DepartmentsController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Department newDepartment)
+        {
+            await _repo.AddAsync(newDepartment);
+            await _repo.SaveAsync();
+
+            return CreatedAtAction(nameof(GetDepartmentByID), new { id = newDepartment.ID }, newDepartment);
+        }
+
+        // PUT api/<DepartmentsController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Department department)
+        {
+            if (id != department.ID)
+                return BadRequest();
+
+            _repo.Update(department);
+            await _repo.SaveAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/<DepartmentsController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            _repo.Delete(id);
+            await _repo.SaveAsync();
+
+            return NoContent();
+        }
+    }
+}
